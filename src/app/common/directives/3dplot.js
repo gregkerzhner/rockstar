7f02f26452bd2505a503fbc6d94b007cd2c34562
Greg Kerzhner <greg@generalthings.com>
2014-05-21 07:49:54 -0600

    GK: showing some graph"

--- src/app/common/directives/3dplot.js
+++ src/app/common/directives/3dplot.js
@@ -16,20 +16,26 @@ angular.module('rockstar.common.directives.3dplot', [])
     $scope.attempt;
     var height = 600;
     var width = 600;
+    var scale = 1000000000000000;
+    var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, scale);
+    camera.position.z =  1637.7542724609375 * scale ;
+    camera.position.x = 204.37328874992676 * scale + 50;
+    camera.position.y = 135.516586103776 *  scale + 50;
 
-    var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
     var scene = new THREE.Scene();    
+    scene.add(new THREE.AmbientLight(0xeeeeee));
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height);
     document.getElementById($element.attr('id')).appendChild(renderer.domElement);
 
     var terrainSize = 60;
-    var projection = d3.geo.transverseMercator()
-      .translate([terrainSize / 2, terrainSize / 2])
-      .scale(terrainSize * 106.4)
+    var projection = d3.geo.mercator();
+      //.translate([terrainSize / 2, terrainSize / 2])
+      //.scale(terrainSize * 106.4)
       //.rotate([-9, 0, 0])
-      .center([-105.28164850466801, 40.01374124251033]); 
-    camera.position.set(0, -terrainSize / 2, terrainSize / 2);
+      //.center([-105.28164850466801, 40.01374124251033]); 
+    //camera.position.set(0, -terrainSize / 2, terrainSize / 2);
 
     $scope.draw = function(){
       if(!$scope.attempt) return;
@@ -37,8 +43,10 @@ angular.module('rockstar.common.directives.3dplot', [])
       var geometry = new THREE.Geometry();
       for(var i = 0 ; i<coordinates.length; i++){
         var coordinate = coordinates[i];
-        coord = $scope.translate(projection([coordinate.longitude, coordinate.latitude]));
-        geometry.vertices.push(new THREE.Vector3(coord[0], coord[1], coordinate.altitude)); 
+        console.log("Before projection x:  %f y: %f z: %f", coordinate.longitude, coordinate.latitude, coordinate.altitude);
+        coord = $scope.translate(projection([coordinate.longitude, coordinate.latitude]));  
+        console.log("Adding point x:  %f y: %f z: %f", coord[0] * scale, coord[1] * scale, coordinate.altitude * scale);
+        geometry.vertices.push(new THREE.Vector3(coord[0] * scale, coord[1] * scale, coordinate.altitude * scale)); 
       }
       var material = new THREE.LineBasicMaterial({
         color: 0xffffff,
@@ -46,7 +54,6 @@ angular.module('rockstar.common.directives.3dplot', [])
       });
 
       var line = new THREE.Line(geometry, material);
-      debugger;
       scene.add(line);
 
       $scope.render()
@@ -59,7 +66,8 @@ angular.module('rockstar.common.directives.3dplot', [])
     }
 
     $scope.translate = function(point) {
-      return [point[0] - (terrainSize / 2), (terrainSize / 2) - point[1]];
+      return point;
+      //return [point[0] - (terrainSize / 2), (terrainSize / 2) - point[1]];
     }
 
     $scope.$watch('attempt', $scope.draw)   
